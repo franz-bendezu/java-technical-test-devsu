de1bba156efbe23411dffcaab5afa4b19d3cc9c6
Franz Bendezu <franzant20@gmail.com>
2024-08-27 03:16:14 -0500

    fea(account-ms): add findAllByClientId method to AccountRepository

--- account-service/src/main/java/com/devsu/bank/account_service/repository/AccountRepository.java
+++ account-service/src/main/java/com/devsu/bank/account_service/repository/AccountRepository.java
@@ -3,10 +3,16 @@ package com.devsu.bank.account_service.repository;
 import org.springframework.stereotype.Repository;
 
 import com.devsu.bank.account_service.model.Account;
+import com.devsu.bank.account_service.model.Transaction;
+
+import java.util.Collection;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+    List<Account> findAllByClientId(Long clientId);
 
 }
