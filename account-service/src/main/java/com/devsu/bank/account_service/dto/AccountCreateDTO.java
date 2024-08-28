bd0bcd45be2402977eed33c1a8cb90ee77aa6b49
Franz Bendezu <franzant20@gmail.com>
2024-08-27 19:23:25 -0500

    refactor: add validation annotations to DTO classes

--- account-service/src/main/java/com/devsu/bank/account_service/dto/AccountCreateDTO.java
+++ account-service/src/main/java/com/devsu/bank/account_service/dto/AccountCreateDTO.java
@@ -1,18 +1,26 @@
 package com.devsu.bank.account_service.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 
 @Data
 public class AccountCreateDTO {
 
+    @NotNull
     private String accountNumber;
 
+    @NotNull
     private String accountType;
 
+    @NotNull
+    @Min(0)
     private Integer initialAmount;
 
+    @NotNull
     private boolean status;
 
+    @NotNull
     private Long clientId;
 
 }
--- account-service/src/main/java/com/devsu/bank/account_service/dto/TransactionCreateDTO.java
+++ account-service/src/main/java/com/devsu/bank/account_service/dto/TransactionCreateDTO.java
@@ -1,12 +1,16 @@
 package com.devsu.bank.account_service.dto;
 
+import org.springframework.lang.NonNull;
+
 import lombok.Data;
 
 @Data
 public class TransactionCreateDTO {
 
+    @NonNull
     private Integer amount;
 
+    @NonNull
     private Long accountId;
 
     public TransactionCreateDTO() {
