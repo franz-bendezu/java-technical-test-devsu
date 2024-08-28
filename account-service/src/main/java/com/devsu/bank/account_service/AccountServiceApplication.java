25a99a268c72d20a18e465f8637c82d8a2a68bb5
Franz Bendezu <franzant20@gmail.com>
2024-08-28 01:38:21 -0500

    refactor(account-ms): enable JPA auditing for Transaction entity

--- account-service/src/main/java/com/devsu/bank/account_service/AccountServiceApplication.java
+++ account-service/src/main/java/com/devsu/bank/account_service/AccountServiceApplication.java
@@ -2,8 +2,10 @@ package com.devsu.bank.account_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AccountServiceApplication {
 
 	public static void main(String[] args) {
