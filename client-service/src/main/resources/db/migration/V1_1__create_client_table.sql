d6f833f041b9f4fcad05c9095bc4fb803df5cb6f
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-29 07:58:27 +0000

    refactor(client-ms): set person and client table schema

--- client-service/src/main/resources/db/migration/V1_1__create_client_table.sql
+++ client-service/src/main/resources/db/migration/V1_1__create_client_table.sql
@@ -1,8 +1,8 @@
 CREATE TABLE client (
     id SERIAL PRIMARY KEY,
-    password VARCHAR(255),
-    status boolean,
-    person_id INT,
+    password VARCHAR(255) NOT NULL,
+    status boolean DEFAULT false,
+    person_id INT NOT NULL,
     CONSTRAINT fk_person
         FOREIGN KEY(person_id) 
         REFERENCES person(id)
