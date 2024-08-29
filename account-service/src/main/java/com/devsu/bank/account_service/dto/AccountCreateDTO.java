e81fe417170a920c2f4399f67e779a515dcac084
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-29 01:29:52 +0000

    refactor(account-ms): update account DTOs with JSON property annotations for map spanish json

--- account-service/src/main/java/com/devsu/bank/account_service/dto/AccountCreateDTO.java
+++ account-service/src/main/java/com/devsu/bank/account_service/dto/AccountCreateDTO.java
@@ -1,6 +1,9 @@
 package com.devsu.bank.account_service.dto;
 
 import jakarta.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Min;
 import lombok.Data;
 
@@ -8,19 +11,24 @@ import lombok.Data;
 public class AccountCreateDTO {
 
     @NotNull
+    @JsonProperty("numeroCuenta")
     private String accountNumber;
 
     @NotNull
+    @JsonProperty("tipo")
     private String accountType;
 
     @NotNull
     @Min(0)
+    @JsonProperty("montoInicial")
     private Integer initialAmount;
 
     @NotNull
+    @JsonProperty("estado")
     private boolean status;
 
     @NotNull
+    @JsonProperty("cliente")
     private Long clientId;
 
 }
--- account-service/src/main/java/com/devsu/bank/account_service/dto/AccountDTO.java
+++ account-service/src/main/java/com/devsu/bank/account_service/dto/AccountDTO.java
@@ -1,20 +1,28 @@
 package com.devsu.bank.account_service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class AccountDTO {
 
+    @JsonProperty("id")
     private Long id;
 
+    @JsonProperty("numeroCuenta")
     private String accountNumber;
 
+    @JsonProperty("tipo")
     private String accountType;
 
+    @JsonProperty("montoInicial")
     private Integer initialAmount;
 
+    @JsonProperty("estado")
     private boolean status;
 
+    @JsonProperty("cliente")
     private Long clientId;
 
 }
