d1fe4f2cee01ec74b8eb85393dc424f151742bc1
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-29 08:00:07 +0000

    refactor(client-ms): update client table schema and foreign key reference

--- client-service/src/main/resources/db/migration/V1_1__create_client_table.sql
+++ client-service/src/main/resources/db/migration/V1_1__create_client_table.sql
@@ -2,8 +2,7 @@ CREATE TABLE client (
     id SERIAL PRIMARY KEY,
     password VARCHAR(255) NOT NULL,
     status boolean DEFAULT false,
-    person_id INT NOT NULL,
     CONSTRAINT fk_person
-        FOREIGN KEY(person_id) 
+        FOREIGN KEY(id) 
         REFERENCES person(id)
 );
