f773b4ce203b675e6c3a6556556cf2f581ba0a7f
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-29 21:30:30 +0000

    refactor(account-ms): update DTOs with JSON property names for customer, accounts, account number, initial amount, and transactions

--- account-service/src/main/java/com/devsu/bank/account_service/dto/StatementAccountDTO.java
+++ account-service/src/main/java/com/devsu/bank/account_service/dto/StatementAccountDTO.java
@@ -2,15 +2,20 @@ package com.devsu.bank.account_service.dto;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class StatementAccountDTO {
 
+    @JsonProperty("numeroCuenta")
     private String accountNumber;
 
+    @JsonProperty("saldoInicial")
     private Integer initialAmount;
 
+    @JsonProperty("movimientos")
     private List<TransactionDTO> transactions;
 
 }
