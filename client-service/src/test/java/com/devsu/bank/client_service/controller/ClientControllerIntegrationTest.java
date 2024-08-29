373221970b1409640ee430e4b88bd3986ff0de5f
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-29 07:52:03 +0000

    refactor(client-ms): update ClientController to use ClientCreateDTO for save and update methods

--- client-service/src/test/java/com/devsu/bank/client_service/controller/ClientControllerIntegrationTest.java
+++ client-service/src/test/java/com/devsu/bank/client_service/controller/ClientControllerIntegrationTest.java
@@ -110,6 +110,7 @@ public class ClientControllerIntegrationTest {
         client.setAddress("123 Main St");
         client.setPhone("555-1234");
         client.setPassword("password");
+        client.setStatus("false");
 
         mockMvc.perform(post("/clientes")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -127,6 +128,7 @@ public class ClientControllerIntegrationTest {
         client.setAddress("456 Main St");
         client.setPhone("555-5678");
         client.setPassword("password");
+        client.setStatus("true");
 
         mockMvc.perform(put("/clientes/{id}", initialClient.getId())
                 .contentType(MediaType.APPLICATION_JSON)
