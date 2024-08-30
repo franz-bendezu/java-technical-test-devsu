03f84a3cbdb8547d8dae04af28e1b1496e9f1233
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-30 07:50:03 +0000

    refactor: handle validation errors in GlobalExceptionHandler

--- client-service/src/main/java/com/devsu/bank/client_service/controller/GlobalExceptionHandler.java
+++ client-service/src/main/java/com/devsu/bank/client_service/controller/GlobalExceptionHandler.java
@@ -1,23 +1,53 @@
 package com.devsu.bank.client_service.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
+import com.devsu.bank.client_service.dto.ErrorResponse;
+import com.devsu.bank.client_service.dto.ErrorValidationResponse;
 import com.devsu.bank.client_service.exception.ClientNotFoundException;
-
 
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ClientNotFoundException.class)
-    public ResponseEntity<ClientNotFoundException> handleRuntimeException(ClientNotFoundException ex) {
-        // Directly return a message or status code
-        return new ResponseEntity<>(ex, HttpStatus.NOT_FOUND);
+    public ResponseEntity<ErrorResponse> handleRuntimeException(ClientNotFoundException ex, WebRequest request) {
+        return new ResponseEntity<>(
+                new ErrorResponse(ex.getMessage(),
+                        request.getDescription(false), ex.getCode()),
+                HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorValidationResponse> handleValidationExceptions(
+            MethodArgumentNotValidException ex, WebRequest request) {
+
+        List<ErrorValidationResponse.ValidationError> errors = ex.getBindingResult()
+                .getAllErrors()
+                .stream()
+                .map(error -> {
+                    String fieldName = ((FieldError) error).getField();
+                    String errorMessage = error.getDefaultMessage();
+                    ErrorValidationResponse.ValidationError validationError = new ErrorValidationResponse.ValidationError(
+                            fieldName, errorMessage);
+                    return validationError;
+                })
+                .collect(Collectors.toList());
+
+        ErrorValidationResponse errorResponse = new ErrorValidationResponse(
+                "Campos inválidos", request.getDescription(false), errors);
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }
--- client-service/src/main/java/com/devsu/bank/client_service/exception/ClientNotFoundException.java
+++ client-service/src/main/java/com/devsu/bank/client_service/exception/ClientNotFoundException.java
@@ -2,6 +2,9 @@ package com.devsu.bank.client_service.exception;
 
 public class ClientNotFoundException  extends RuntimeException {
     private static final String DEFAULT_MESSAGE = "Cliente no encontrado";
+    private static final String DEFAULT_CODE = "CLIENT_NOT_FOUND";
+
+    private String code = DEFAULT_CODE;
 
     public ClientNotFoundException() {
         super(DEFAULT_MESSAGE);
@@ -9,5 +12,9 @@ public class ClientNotFoundException  extends RuntimeException {
 
     public ClientNotFoundException(String message) {
         super(message);
+    }
+
+    public String getCode() {
+        return code;
     }
 }
