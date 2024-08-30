3c99df41c955c8ceb75b31ccfe0344286ff24ac6
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-30 07:39:44 +0000

    refactor: handle validation errors in GlobalExceptionHandler

--- account-service/src/main/java/com/devsu/bank/account_service/controller/GlobalExceptionHandler.java
+++ account-service/src/main/java/com/devsu/bank/account_service/controller/GlobalExceptionHandler.java
@@ -1,14 +1,21 @@
 package com.devsu.bank.account_service.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import com.devsu.bank.account_service.dto.ErrorResponse;
+import com.devsu.bank.account_service.dto.ErrorValidationResponse;
 import com.devsu.bank.account_service.exception.AccountNotFoundException;
 import com.devsu.bank.account_service.exception.ClientNotFoundException;
 import com.devsu.bank.account_service.exception.InsufficientBalanceException;
@@ -46,6 +53,28 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity<>(
 				new ErrorResponse(ex.getMessage(), request.getDescription(false), ex.getCode()),
 				HttpStatus.NOT_FOUND);
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ErrorValidationResponse> handleValidationExceptions(
+			MethodArgumentNotValidException ex, WebRequest request) {
+
+		List<ErrorValidationResponse.ValidationError> errors = ex.getBindingResult()
+				.getAllErrors()
+				.stream()
+				.map(error -> {
+					String fieldName = ((FieldError) error).getField();
+					String errorMessage = error.getDefaultMessage();
+					ErrorValidationResponse.ValidationError validationError = new ErrorValidationResponse.ValidationError(
+							fieldName, errorMessage);
+					return validationError;
+				})
+				.collect(Collectors.toList());
+
+		ErrorValidationResponse errorResponse = new ErrorValidationResponse(
+				"Campos inválidos", request.getDescription(false), errors);
+
+		return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
 	}
 
 }
