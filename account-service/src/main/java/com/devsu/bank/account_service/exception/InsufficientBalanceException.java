87c90b257291c583cea2fd57b02daf1227c35cb4
Franz Bendezu <63468778+franz-bendezu@users.noreply.github.com>
2024-08-30 07:28:07 +0000

    refactor: set exception classes to include custom error codes

--- account-service/src/main/java/com/devsu/bank/account_service/exception/InsufficientBalanceException.java
+++ account-service/src/main/java/com/devsu/bank/account_service/exception/InsufficientBalanceException.java
@@ -3,6 +3,17 @@ package com.devsu.bank.account_service.exception;
 public class InsufficientBalanceException extends RuntimeException {
     private static final String DEFAULT_MESSAGE = "Saldo insuficiente";
 
+    private static final String DEFAULT_CODE = "INSUFFICIENT_BALANCE";
+    private String code = DEFAULT_CODE;
+
+    public InsufficientBalanceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InsufficientBalanceException(Throwable cause) {
+        super(DEFAULT_MESSAGE, cause);
+    }
+
     public InsufficientBalanceException() {
         super(DEFAULT_MESSAGE);
     }
@@ -10,4 +21,10 @@ public class InsufficientBalanceException extends RuntimeException {
     public InsufficientBalanceException(String message) {
         super(message);
     }
+
+
+    public String getCode() {
+        return code;
+    }
+    
 }
